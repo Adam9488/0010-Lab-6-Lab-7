3ebada810e95417181b0de236a1b709db1a7507f
David Pultynovich <dzp7@pitt.edu>
2023-02-14 17:03:41 -0500

    Made something.py do something.

--- something.py
+++ something.py
@@ -1,1 +1,12 @@
 print("Something")
+
+name = input("Please input a name. ")
+print("Hello, " + name + ".")
+
+phrase = input("Please input a string. ")
+reverse = ""
+
+for letter in phrase:
+	reverse = letter + reverse
+
+print(reverse)
